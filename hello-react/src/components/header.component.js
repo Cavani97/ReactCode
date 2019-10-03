add3f6a752638341c9b346efb96bb1298ee826dd
Avani <avanichawla229703@gmail.com>
2019-10-03 18:05:54 +0530

    header changes

--- hello-react/src/components/header.component.js
+++ hello-react/src/components/header.component.js
@@ -6,7 +6,7 @@ function header(props) {
     return (<div className='header'>
             <h1>{props.title}</h1>
             <ul>
-                <li><a href={props.baseUrl}>Home</a></li>
+                <li><a href={props.baseUrl}>New Committ</a></li>
                 <li><a href={props.baseUrl+'/about'}>About</a></li>
                 <li><a href={props.baseUrl+'/contact'}>Contact</a></li>
             </ul>
